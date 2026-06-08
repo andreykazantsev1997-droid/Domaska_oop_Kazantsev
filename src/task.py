666ae68955fd05d1bcbaedddf9a46cf30fd31513
Andrey Kazantsev <andrey.kazantsev.1997@gmail.com>
2026-06-08 13:06:25 +0300

    add main.py 14_2 and update task.py

--- src/task.py
+++ src/task.py
@@ -1,9 +1,32 @@
+from itertools import product
+
+
 class Product:
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product_dict: dict):
+        return cls(
+            name=product_dict["name"],
+            description=product_dict["description"],
+            price=product_dict["price"],
+            quantity=product_dict["quantity"]
+        )
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = new_price
 
 
 class Category:
@@ -13,6 +36,20 @@ class Category:
     def __init__(self, name: str, description: str, products: list):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self, product: int):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        product_string = []
+        for product in self.__products:
+            info = f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт."
+            product_string.append(info)
+        return product_string
+
+
