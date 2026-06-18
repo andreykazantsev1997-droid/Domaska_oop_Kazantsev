01c739358d5493e89953e6c31e2a325db23daaee
Andrey Kazantsev <andrey.kazantsev.1997@gmail.com>
2026-06-18 18:56:40 +0300

    add new func and new main.py

--- src/task.py
+++ src/task.py
@@ -29,6 +29,8 @@ class Product:
         return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
 
     def __add__(self, other):
+        if type(self) != type(other):
+            raise TypeError
         sum_price = (self.price * self.quantity) + (other.price * other.quantity)
         return sum_price
 
@@ -45,6 +47,8 @@ class Category:
         Category.product_count += len(products)
 
     def add_product(self, product: int):
+        if not isinstance(product, Product) or not issubclass(type(product), Product):
+            raise TypeError
         self.__products.append(product)
         Category.product_count += 1
 
@@ -52,3 +56,19 @@ class Category:
     def products(self):
         total_quantity = sum(product.quantity for product in self.__products)
         return f"{self.name}, количество продуктов: {total_quantity} шт."
+
+class Smartphone(Product):
+    def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+
+class LawnGrass(Product):
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
