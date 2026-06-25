a9cd21fb01a1fcd9c18eaa477cd12e2d0990b697
Andrey Kazantsev <andrey.kazantsev.1997@gmail.com>
2026-06-25 19:42:19 +0300

    update tests.py and add new tests. Fix code with linters

--- 17.1_main.py
+++ 17.1_main.py
@@ -1,13 +1,13 @@
 from src.task import Product, Category
 
 if __name__ == '__main__':
-    # try:
-    #     product_invalid = Product("Бракованный товар", "Неверное количество", 1000.0, 0)
-    # except ValueError as e:
-    #     print(
-    #         "Возникла ошибка ValueError прерывающая работу программы при попытке добавить продукт с нулевым количеством")
-    # else:
-    #     print("Не возникла ошибка ValueError при попытке добавить продукт с нулевым количеством")
+    try:
+        product_invalid = Product("Бракованный товар", "Неверное количество", 1000.0, 0)
+    except ValueError as e:
+        print(
+            "Возникла ошибка ValueError прерывающая работу программы при попытке добавить продукт с нулевым количеством")
+    else:
+        print("Не возникла ошибка ValueError при попытке добавить продукт с нулевым количеством")
 
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
